11720c1f91299094345047c79b350c9879c09423
IMSRS <53367382+theunhackable@users.noreply.github.com>
2023-04-19 22:25:08 +0530

    added all files to github

---
+++ frontend-libraries/markdown-previewer/markdown-preview/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Main from './components/main';
+import NavBar from './components/NavBar';
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
